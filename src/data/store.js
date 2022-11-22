e11a623e281f63e445f82e4295cee45021362e9c
FR_Jess <fr_jess@icloud.com>
2022-11-22 10:53:38 +0100

    [improve] store

--- src/data/store.js
+++ src/data/store.js
@@ -1,10 +1,17 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-  apiUrlMovies: 'https://api.themoviedb.org/3/search/movie/',
-  apiUrlTvShows: 'https://api.themoviedb.org/3/search/tv/',
-  apiKey: '06a0961b60d2024ca572796f493b8635',
-  movieShowSearch: '',
+  apiUrl: 'https://api.themoviedb.org/3/search/',
+  // apiUrlTvShows: 'https://api.themoviedb.org/3/search/tv/',
+  apiParams: {
+    apiKey: '06a0961b60d2024ca572796f493b8635',
+    movieShowSearch: '',
+    language: 'it-IT',
+  },
   moviesList: [],
   tvShowsList: [],
+  type: '',
 })
+
+
+//una sola card sia movie che show perché è inutile scrivere 2 volte lo stesso code
